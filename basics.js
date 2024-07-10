842604d0246c8a9c4df9a2301a844a88a2b689fc
notStinG <119837267+notStinG@users.noreply.github.com>
2024-07-10 09:10:53 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -1,1 +1,23 @@
+let name = "Gerrard";
+const myAura = 5000000;
+let rankPoints = 27;
+rankPoints = 70
+let reportCard = ["A", "A", "A", "A", "A", "A", "A"]
+let radiant = true;
 
+console.log(name);
+console.log(myAura);
+console.log(rankPoints);
+console.log(reportCard);
+console.log(radiant);
+
+let a = 5 + 7;
+let b = 6 - 4;
+let c = 3 * 9;
+let d = 8 / 2;
+let e = 10 ** 7;
+let f = 49 % 7;
+let g = Math.random()
+let h = Math.round(818292.18829191)
+
+console.log(a, b, c, d, e, f, g, h)
