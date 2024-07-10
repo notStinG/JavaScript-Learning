1260939d22b586770088f0149235874ca271b340
notStinG <119837267+notStinG@users.noreply.github.com>
2024-07-10 09:38:27 +0800

    Update basics.js

--- basics.js
+++ basics.js
@@ -61,3 +61,29 @@ while (counter > 0) {
     counter--;
 }
 console.log("Submission deadline passed")
+
+
+let num1 = 0;
+let num2 = 1;
+let rep = 10;
+if (rep == 1) {
+    console.log(num1)
+}
+else if (rep == 2) {
+    console.log(num1)
+    console.log(num2)
+}
+else {
+    let fib = [num1, num2];
+}
+
+while (rep > fib.length) {
+    let buffer = num1 + num2;
+    num1 = num2;
+    num2 = buffer;
+    fib.push(num2);
+}
+
+for (let num of fib) {
+    console.log(num);
+}
